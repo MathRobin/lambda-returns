006b2fb9d46f8b8acc04862ec853968438a8d4ac
Mathieu KIM ROBIN <mthrobin@gmail.com>
2024-08-13 14:19:38 +0200

    feat(version): bump

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,8 +1,9 @@
-import { OutgoingHttpHeaders } from 'http';
 import { APIGatewayProxyResult } from 'aws-lambda';
 
-type HttpHeaders = OutgoingHttpHeaders & {
-    [header: string]: string | number | boolean;
+type StandardHttpHeaderKey = 'accept' | 'accept-charset' | 'accept-encoding' | 'accept-language' | 'authorization' | 'cache-control' | 'connection' | 'content-length' | 'content-type' | 'cookie' | 'date' | 'expect' | 'forwarded' | 'from' | 'host' | 'if-match' | 'if-modified-since' | 'if-none-match' | 'if-range' | 'if-unmodified-since' | 'origin' | 'pragma' | 'proxy-authorization' | 'range' | 'referer' | 'te' | 'trailer' | 'transfer-encoding' | 'upgrade' | 'user-agent' | 'via' | 'warning';
+type HttpHeaderKey = StandardHttpHeaderKey | (string & object);
+type HttpHeaders = Record<HttpHeaderKey, string | number | boolean> | {
+    [key: string]: string | number | boolean;
 };
 
 type LambdaResponse<TCode extends number> = Omit<APIGatewayProxyResult, 'statusCode' | 'headers'> & {
