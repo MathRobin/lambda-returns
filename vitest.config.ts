d055686313cf2e24e63abde10ebc349da9632052
Mathieu KIM ROBIN <mthrobin@gmail.com>
2026-08-22 18:22:21 +0200

    chore(config): update tooling configuration

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,12 +1,16 @@
+import path from 'node:path';
 import { configDefaults, defineConfig } from 'vitest/config';
-import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@': path.resolve(import.meta.dirname),
+    },
+  },
   test: {
     coverage: {
       provider: 'v8',
       exclude: [...configDefaults.exclude, 'src/scripts/res', 'src/utils/ts'],
     },
   },
-  plugins: [tsconfigPaths()],
 });
